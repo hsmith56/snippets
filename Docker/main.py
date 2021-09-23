9c152bec2b871d3d096c9aeab101af43c43fd4e7
Harrison <47962432+hsmith56@users.noreply.github.com>
2021-09-23 14:59:08 -0400

    Add files via upload

---
+++ Docker/main.py
@@ -0,0 +1,50 @@
+import subprocess
+import time
+
+def start_containers(filename = 'Names.txt'):
+    create_container = 'docker run -dit --name={0} test1 {0}'
+    restart_container = 'docker restart {}'
+
+    containers_to_create = None
+    with open(filename, 'r') as file:
+        containers_to_create = [line.strip() for line in file]
+
+    try:
+        all_docker_containers = subprocess.run('docker ps -a --format "{{.Names}}"', capture_output=True, text=True, universal_newlines=True)
+        all_containers = all_docker_containers.stdout.strip().split('\n')
+        if all_docker_containers.stderr:
+            raise Exception(all_docker_containers.stderr)
+        running_container = subprocess.run('docker ps --format "{{.Names}}"', capture_output=True, text=True, universal_newlines=True)
+        running_containers = running_container.stdout.strip().split('\n')
+
+    except Exception as e:
+        raise Exception(e)
+
+    try:
+        for name in containers_to_create:
+            if name not in all_containers:
+                container_creation = subprocess.run(create_container.format(name), capture_output=True, text=True) 
+                if container_creation.stderr:
+                    raise Exception(container_creation.stderr)
+                print(f'Container "{name}" was created with id: {container_creation.stdout}')
+
+            elif name in all_containers and name not in running_containers:
+                container_restart = subprocess.run(restart_container.format(name), capture_output=True, text=True)
+                print(f'Container "{name}" was restarted')
+
+    except Exception as e:
+        print(e)
+
+while True:
+    try:
+        start_containers()
+    except Exception as e:
+        print(e)
+    print('waiting 10 seconds...')
+    time.sleep(10)
+
+# stop_container = 'docker stop {}'
+# for container in running_containers:
+#     if container not in containers_to_create:
+#         container_stop = subprocess.run(stop_container.format(name), capture_output=True)
+#         print(f'Container {container_stop.stdout} has been stopped')
