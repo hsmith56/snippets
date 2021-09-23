9bc123efbf5a24f822cd5e25e3deadcf9f62901f
Harrison <47962432+hsmith56@users.noreply.github.com>
2021-09-23 16:35:50 -0400

    Corrected first run logic

--- Docker/main.py
+++ Docker/main.py
@@ -1,15 +1,15 @@
 import subprocess
 import time
 
-def start_containers(filename = 'Names.txt'):
-    create_container = 'docker run -dit --name={0} test1 {0}'
-    restart_container = 'docker restart {}'
-    stop_container = 'docker stop {}'
+create_container = 'docker run -dit --name={0} test1 {0}'
+restart_container = 'docker restart {}'
+stop_container = 'docker stop {}'
 
+def start_containers(filename = 'Names.txt'):
     containers_to_create = None
+    
     with open(filename, 'r') as file:
         containers_to_create = [line.strip() for line in file if line.strip() != ""]
-
 
     try:
         all_docker_containers = subprocess.run('docker ps -a --format "{{.Names}}"', capture_output=True, text=True, universal_newlines=True)
@@ -18,13 +18,13 @@ def start_containers(filename = 'Names.txt'):
             raise Exception(all_docker_containers.stderr)
         running_container = subprocess.run('docker ps --format "{{.Names}}"', capture_output=True, text=True, universal_newlines=True)
         running_containers = running_container.stdout.strip().split('\n')
-
     except Exception as e:
         raise Exception(e)
 
     try:
         for name in containers_to_create:
             # if the container doesn't exist at all, create
+
             if name not in all_containers:
                 container_creation = subprocess.run(create_container.format(name), capture_output=True, text=True) 
                 if container_creation.stderr:
@@ -34,13 +34,17 @@ def start_containers(filename = 'Names.txt'):
             # if the container exists but is stopped, restart
             elif name in all_containers and name not in running_containers:
                 container_restart = subprocess.run(restart_container.format(name), capture_output=True, text=True)
+                if container_restart.stderr:
+                    raise Exception(container_restart.stderr)
                 print(f'Container "{name}" was restarted')
     except Exception as e:
         print(e)
-        # if name not in list, stop the running container
+        
     try:
+        # if name not in list, stop the running container
         for container in running_containers:
-            if container not in containers_to_create:
+            if container not in containers_to_create and container != "":
+                print(f'Trying to stop container: {container}')
                 container_stop = subprocess.run(stop_container.format(container), capture_output=True)
                 if container_stop.stderr:
                     raise Exception(container_stop.stderr)
