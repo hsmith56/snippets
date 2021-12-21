24ea211d51c452c679a7654f605e210a4f03717f
Harrison <47962432+hsmith56@users.noreply.github.com>
2021-12-21 19:30:58 +0000

    changed print statements to tests

--- json_stream.py
+++ json_stream.py
@@ -10,7 +10,7 @@ import logging
 def create_json(rnge_start, rnge_end, function):
     with open('badjson.txt', function) as f:
         for i in range(rnge_start, rnge_end):
-            f.write('{"id": '+ f'"{i}"' + ', "name": "sid", "dataTypeName": "meta_data", "fieldName": ":sid", "position": 0, "renderTypeName": "meta_data", "format": {}, "flags": ["hidden"]}\n')
+            f.write('{"id": '+ f'"{i}"' + ', "name": "sid", "Random thing here": "random value here as well","dataTypeName": "meta_data", "fieldName": ":sid", "position": 0, "renderTypeName": "meta_data", "format": {}, "flags": ["hidden"]}\n')
 
 create_json(rnge_start=0, rnge_end=1_000_000, function='w+')
 
@@ -24,28 +24,28 @@ class Connector:
 
 
     def stream_read_json(self, decoder=JSONDecoder(), buffer_size = 8192):
-        self.curr_file_size = os.stat(self.output_file).st_size # Get the size of the file in bytes
+        # Get the size of the file in bytes
+        self.curr_file_size = os.stat(self.output_file).st_size
 
         with open(self.output_file, 'r') as fileobj:
-            # if the file has increased in size, go to last read position
-            # otherwise start from the beginning
+            # If the file has increased in size, go to last read position
+            # in order to avoid rereading lines, otherwise start from the beginning
             if self.curr_file_size > self.prev_file_size:
                 fileobj.seek(self.prev_file_size)
-
-            
             buffer = ""
 
             for chunk in iter(partial(fileobj.read, buffer_size), ""):
                 buffer += chunk
-                # MUST HAVE THIS LINE OTHERWIES IT WILL ALWAYS GET STUCK AND INFINITELY INCREASE BUFFER
+                # MUST HAVE THIS LINE OTHERWIES IT ALMOST ALWAYS
+                # GETS STUCK AND INFINITELY INCREASES BUFFER
                 buffer = buffer.strip()
 
                 while buffer:
                     try:
                         result, index = decoder.raw_decode(buffer)
                     
-                        # this is for my fake JSON, do actual stuff here
-                        self.current_offset = abs(int(result['id']))
+                        # This is for my fake JSON, do actual stuff here
+                        self.current_offset = int(result['id'])
                         if self.current_offset > self.prev_offset:
                             self.prev_offset = self.current_offset
                             yield result
@@ -53,31 +53,35 @@ class Connector:
                         buffer = buffer[index:].lstrip()
                     except ValueError as e:
                         break
+
         self.prev_file_size = self.curr_file_size
 
 t1 = Connector()
 test = t1.stream_read_json()
-last = ""
-
-# Proof that every single item gets iterated over
-# last would not be 999_999 if it failed previously
+# On first read file is 1_000_000 lines long, each id corresponds
+# to the index so we can easily test if the object was converted to JSON or not
 for index, i in enumerate(test):
-    last = i
-print(last)
+    assert i['id'] == str(index), f"Failed to match index {index} to JSON in Test 1"
+else:
+    print('All Tests passed for initial read of 1,000,000 lines')
 
 
-# append 1_000_000:1_000_020 to the json
 create_json(rnge_start=1_000_000, rnge_end=1_000_020, function='a')
+# append 1_000_000:1_000_020 to the json
+# The file will increase in size so this
+# should only read from the new entires onwards
 
-# The file has since increased in size so this should only read from the new entires onwards
 test = t1.stream_read_json()
-for i in test:
-    print(i)
+for index, i in enumerate(test):
+    assert i['id'] == str(index+1_000_000), f"Failed to match index {index} to JSON in Test 2"
+print('All Tests passed for appending to file and starting from correct location')
 
-# Overwrite the whole file starting at 1_000_020 going forward
+
 create_json(rnge_start=1_000_020, rnge_end=1_000_050, function='w')
+# Overwrite the whole file starting at 1_000_020 going forward
+# Since the file was rewritten, start from beginning of file
 
-# Since the file was rewritter, start from 0
 test = t1.stream_read_json()
-for i in test:
-    print(i)
+for index, i in enumerate(test):
+    assert i['id'] == str(index+1_000_020), f"Failed to match index {index} to JSON in Test 3"
+print('All Tests passed for new file being written to and starting over')
